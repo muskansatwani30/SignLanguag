8c3e41aabf18760b0c5c425c6df017e31545959c
muskansatwani30 <112712208+muskansatwani30@users.noreply.github.com>
2022-12-02 22:42:03 +0530

    Add files via upload

---
+++ App.py
@@ -0,0 +1,79 @@
+import numpy as np
+from keras.models import model_from_json
+import operator
+import cv2
+import sys, os
+import streamlit as st
+
+st.markdown("<h1 style='text-align: center; color: #483C32;'>Number Sign Detection !</h1>", unsafe_allow_html=True)
+#st.title("Number Sign Detection !")
+st.markdown("<h4 style='text-align: center;'>Webcam Application</h4>", unsafe_allow_html=True)
+#st.subheader('Webcam Application')
+st.write('Click the checkbox below to start the webcam.')
+run = st.checkbox('Start')
+FRAME_WINDOW = st.image([])
+
+
+# Loading the model
+json_file = open("model-bw.json", "r")
+model_json = json_file.read()
+json_file.close()
+loaded_model = model_from_json(model_json)
+# load weights into new model
+loaded_model.load_weights("model-bw.h5")
+print("Loaded model from disk")
+
+cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+FRAME_WINDOW = st.image([])
+
+# Category dictionary
+categories = {0: 'ZERO', 1: 'ONE', 2: 'TWO', 3: 'THREE', 4: 'FOUR', 5: 'FIVE'}
+
+while run:
+    _, frame = cap.read()
+    # Simulating mirror image
+    frame = cv2.flip(frame, 1)
+
+    # Got this from collect-data.py
+    # Coordinates of the ROI
+    x1 = int(0.5 * frame.shape[1])
+    y1 = 10
+    x2 = frame.shape[1] - 10
+    y2 = int(0.5 * frame.shape[1])
+    # Drawing the ROI
+    # The increment/decrement by 1 is to compensate for the bounding box
+    cv2.rectangle(frame, (x1 - 1, y1 - 1), (x2 + 1, y2 + 1), (255, 0, 0), 1)
+    # Extracting the ROI
+    roi = frame[y1:y2, x1:x2]
+
+    # Resizing the ROI so it can be fed to the model for prediction
+    roi = cv2.resize(roi, (64, 64))
+    roi = cv2.cvtColor(roi, cv2.COLOR_BGR2GRAY)
+    _, test_image = cv2.threshold(roi, 120, 255, cv2.THRESH_BINARY)
+    cv2.imshow("test", test_image)
+    # Batch of 1
+    result = loaded_model.predict(test_image.reshape(1, 64, 64, 1))
+    prediction = {'ZERO': result[0][0],
+                  'ONE': result[0][1],
+                  'TWO': result[0][2],
+                  'THREE': result[0][3],
+                  'FOUR': result[0][4],
+                  'FIVE': result[0][5]}
+    # Sorting based on top prediction
+    prediction = sorted(prediction.items(), key=operator.itemgetter(1), reverse=True)
+
+    # Displaying the predictions
+    cv2.putText(frame, prediction[0][0], (10, 120), cv2.FONT_HERSHEY_COMPLEX, 1.7, (255, 255, 255), 2)
+    cv2.imshow("Frame", frame)
+    FRAME_WINDOW.image(frame)
+
+    interrupt = cv2.waitKey(10)
+    if interrupt & 0xFF == 27:  # esc key
+        break
+#else:
+    #st.write('Stopped')
+
+
+cap.release()
+cv2.destroyAllWindows()
+
---
+++ pages/Home.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.title("       Number Sign Detection !")
+st.write("Communication is the process of exchange of thoughts and messages in various ways such as speech, signals, behavior and visuals. Deaf and dumb(D&M) people make use of their hands to express different gestures to express their ideas with other people.")
+st.write('In our project we basically focus on producing a model which can recognize. Fingerspelling based hand gestures in order to recognize the numbers.')
+st.write('Communication is the process of exchange of thoughts and messages in various ways such as speech, signals, behavior and visuals. Deaf and dumb(D&M) people make use of their hands to express different gestures to express their ideas with other people.')
+st.write('In our project we basically focus on producing a model which can recognize. Fingerspelling based hand gestures in order to recognize the numbers.')
